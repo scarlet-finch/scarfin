8f09b46edf788eea79a0fd42b1eeb13f0e9cf1c6
Rohitt Vashishtha <aero31aero@gmail.com>
2020-09-14 11:05:14 +0530

    mount: Improve default chronological mounts.

--- src/commands/mount.js
+++ src/commands/mount.js
@@ -12,15 +12,34 @@ const make_date_folders = (files) => {
         const date = moment(file.date_taken);
         paths.push({
             from: file.File.path,
-            to: `${date.year()}/${date.format(
-                'MMMM'
-            )}/${date.date()}-${date.format('dddd')}/${file.uuid}.jpg`,
+            to: `${date.year()}/${date.format('MMMM')}/${date.format(
+                'DD'
+            )}-${date.format('dddd')}/${path.basename(file.File.path)}`,
         });
     }
     return paths;
 };
 
-const create_links = async (pairs) => {
+const make_single_folder = (files) => {
+    const paths = [];
+    for (file of files) {
+        const date = moment(file.date_taken);
+        paths.push({
+            from: file.File.path,
+            to: `flat/${date.toISOString()}.jpg`,
+        });
+    }
+    return paths;
+};
+
+const create_links = async (pairs, location) => {
+    pairs = pairs.map((e) => {
+        e.to = path.join(location, e.to);
+        return e;
+    });
+    for (e of pairs) {
+        _logger.debug(`- ${e.to}`);
+    }
     for (e of pairs) {
         await filelink(e.from, e.to, {
             force: true,
@@ -46,14 +65,10 @@ module.exports = async (opts, flags) => {
             include: [db.Files],
         });
         let pairs = make_date_folders(files);
-        pairs = pairs.map((e) => {
-            e.to = path.join(location, e.to);
-            return e;
-        });
-        for (e of pairs) {
-            _logger.debug(`- ${e.to}`);
-        }
-        await create_links(pairs);
+
+        await create_links(pairs, location);
+        pairs = make_single_folder(files);
+        await create_links(pairs, location);
     } else {
         _logger.alert('specify mount directory');
         _logger.alert('--root /path/to/folder');
