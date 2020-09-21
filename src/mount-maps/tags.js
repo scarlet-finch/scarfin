5e796c58e24e0a2866d620988fcae89b045eb997
Rohitt Vashishtha <aero31aero@gmail.com>
2020-09-22 01:39:45 +0530

    maps: Add tags mount map.

---
+++ src/mount-maps/tags.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const name = 'tags';
+const description = 'create folders by tags';
+const map = (files) => {
+    const pairs = [];
+    for (file of files) {
+        for (tag of file.tags) {
+            pairs.push({
+                from: file.path,
+                to: `${tag}/${path.basename(file.path)}`,
+            });
+        }
+    }
+    return pairs;
+};
+
+// This bit is important to let scarfin import the files.
+module.exports = {
+    name,
+    description,
+    map,
+};
